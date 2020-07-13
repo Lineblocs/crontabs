86c2c8d8f984c85c6ad0c80009ab055b1e60b56a
Nadir Hamid <matrix.nad@gmail.com>
2020-07-12 19:53:17 -0600

    test billing scripts

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module bitbucket.org/infinitet3ch/lineblocs-crontabs
 go 1.14
 
 require (
-	bitbucket.org/infinitet3ch/lineblocs-go-helpers v0.0.0-20200712061129-8303ce0bee26 // indirect
+	bitbucket.org/infinitet3ch/lineblocs-go-helpers v0.0.0-20200713014700-970aef923ef8 // indirect
 	github.com/go-chi/chi v4.1.2+incompatible // indirect
 	github.com/hoisie/mustache v0.0.0-20160804235033-6375acf62c69 // indirect
 	github.com/jinzhu/now v1.1.1 // indirect
--- main.go
+++ main.go
@@ -112,6 +112,17 @@ func runFreeTrialEnding() {
 			fmt.Println(err)
 			continue
 		}
+		stmt, err := db.Prepare("UPDATE workspaces SET free_trial_reminder_sent = 1 WHERE id = ?")
+		if err != nil {
+			fmt.Printf("could not prepare query..\r\n")
+				continue
+		}
+		_, err = stmt.Exec(workspace.Id)
+		if err != nil {
+			fmt.Printf("error updating DB..\r\n")
+			fmt.Println(err)
+			continue
+		}
 	}
 
 }
@@ -146,7 +157,7 @@ func runMonthlyBilling() {
 			fmt.Printf("error getting workspace ID: " + strconv.Itoa(id) + "\r\n")
 			continue
 		}
-		user, err := lineblocs.GetUserFromDB(id)
+		user, err := lineblocs.GetUserFromDB(creatorId)
 	    if err != nil {
 			fmt.Printf("error getting user ID: " + strconv.Itoa(id) + "\r\n")
 			continue
@@ -161,32 +172,36 @@ func runMonthlyBilling() {
 		}
 		if plan == nil {
 			fmt.Printf("No plan found for user..\r\n")
+			continue
 		}
 		billingInfo, err := lineblocs.GetWorkspaceBillingInfo(workspace)
 		if err != nil {
 			fmt.Printf("Could not get billing info..\r\n")
+			fmt.Println(err)
 			continue
 		}
 
 
 		var didId int
 		var monthlyCosts int
-		results1, err := db.Query("SELECT id, monthly_costs  FROM did_numbers WHERE workspace_id = ?", workspace.Id)
-		if err != nil {
+		results1, err := db.Query("SELECT id, monthly_cost  FROM did_numbers WHERE workspace_id = ?", workspace.Id)
+		if ( err != sql.ErrNoRows && err != nil ) {  //create conference
 			fmt.Printf("Could not get dids info..\r\n")
+			fmt.Println(err)
 			continue
 		}
-
-    	for results.Next() {
-			results.Scan(&didId, &monthlyCosts)
-			stmt, err := db.Prepare("INSERT INTO users_logs (`type`, `cents`, `module_id`, `user_id`, `workspace_id`, `created_at`) VALUES ( ?, ?, ?, ?, ?, ?)")
+		defer results1.Close()
+    	for results1.Next() {
+			results1.Scan(&didId, &monthlyCosts)
+			stmt, err := db.Prepare("INSERT INTO users_debits (`source`, `status`, `cents`, `module_id`, `user_id`, `workspace_id`, `created_at`) VALUES ( ?, ?, ?, ?, ?, ?)")
 			if err != nil {
 				fmt.Printf("could not prepare query..\r\n")
+				fmt.Println(err)
 				continue
 			}
 
 			defer stmt.Close()
-			_, err = stmt.Exec("NUMBER_RENTAL", monthlyCosts, didId, user.Id, workspace.Id, currentTime)
+			_, err = stmt.Exec("NUMBER_RENTAL", "INCOMPLETE", monthlyCosts, didId, user.Id, workspace.Id, currentTime)
 			if err != nil {
 				fmt.Printf("error creating number rental debit..\r\n")
 				continue
@@ -210,13 +225,13 @@ func runMonthlyBilling() {
 		faxCosts := 0.0
 		monthlyNumberRentals :=  0.0
 		invoiceDesc := fmt.Sprintf("LineBlocs invoice for %s", billingInfo.InvoiceDue)
-		results1, err = db.Query("SELECT id, source, module_id, cents, created_at FROM users_debits WHERE user_id = ? AND created_at BETWEEN ? AND ?", workspace.CreatorId, startFormatted, endFormatted);
-		defer results1.Close()
+		results2, err := db.Query("SELECT id, source, module_id, cents, created_at FROM users_debits WHERE user_id = ? AND created_at BETWEEN ? AND ?", workspace.CreatorId, startFormatted, endFormatted);
 		if err != nil {
 			fmt.Printf("error running query..\r\n")
 			fmt.Println(err)
 			return
 		}
+		defer results2.Close()
 		var id int
 		var source string
 		var moduleId int
@@ -225,9 +240,11 @@ func runMonthlyBilling() {
 		usedMonthlyMinutes := plan.MinutesPerMonth
 		usedMonthlyRecordings := plan.RecordingSpace
 		usedMonthlyFax := plan.Fax
-    	for results.Next() {
-			results.Scan(&id, &source, &moduleId, &cents, &created)
+    	for results2.Next() {
+			results2.Scan(&id, &source, &moduleId, &cents, &created)
+			fmt.Printf("scanning in debit source %s\r\n", source);
 			if source == "CALL" {
+				fmt.Printf("getting call %d\r\n", moduleId)
 				call, err := lineblocs.GetCallFromDB(moduleId)
 				if err != nil {
 					fmt.Printf("error running query..\r\n")
@@ -235,7 +252,8 @@ func runMonthlyBilling() {
 					return
 				}
 				duration := call.DurationNumber
-				minutes := duration / 60
+				fmt.Printf("call duration is %d\r\n", duration)
+				minutes := float64(duration / 60)
 				change := usedMonthlyMinutes - minutes;
 				charge, err := computeAmountToCharge(cents, usedMonthlyMinutes, float64(change))
 				if err != nil {
@@ -247,29 +265,30 @@ func runMonthlyBilling() {
 				usedMonthlyMinutes = usedMonthlyMinutes - minutes;
 
 			} else if source == "NUMBER_RENTAL" {
+				fmt.Printf("getting DID %d\r\n", moduleId)
 				did, err := lineblocs.GetDIDFromDB(moduleId)
 				if err != nil {
 					fmt.Printf("error running query..\r\n")
 					fmt.Println(err)
-					return
+					continue
 				}
 
-				monthlyNumberRentals += float64(did.MonthlyCosts)
+				monthlyNumberRentals += float64(did.MonthlyCost)
 			} 
 		}
-		results2, err := db.Query("SELECT id, size, created_at FROM users_debits WHERE user_id = ? AND created_at BETWEEN ? AND ?", workspace.CreatorId, startFormatted, endFormatted);
-		defer results1.Close()
-		if err != nil {
+		results3, err := db.Query("SELECT id, size, created_at FROM recordings WHERE user_id = ? AND created_at BETWEEN ? AND ?", workspace.CreatorId, startFormatted, endFormatted);
+		if ( err != sql.ErrNoRows && err != nil ) { 
 			fmt.Printf("error running query..\r\n")
 			fmt.Println(err)
 			return
 		}
+		defer results3.Close()
 		var recId int
-		var size int
+		var size float64
 		var createdAt time.Time
-		for results2.Next() {
-			results2.Scan(&recId, &size, &createdAt)
-			change := float64( usedMonthlyRecordings - size )
+		for results3.Next() {
+			results3.Scan(&recId, &size, &createdAt)
+			change := usedMonthlyRecordings - size
 			cents := math.Round(baseCosts.RecordingsPerByte * float64(size))
 			charge, err := computeAmountToCharge(cents, usedMonthlyRecordings, change)
 			if err != nil {
@@ -281,19 +300,19 @@ func runMonthlyBilling() {
 			usedMonthlyRecordings -= size
 
 		}
-		results3, err := db.Query("SELECT id, created_at FROM faxess WHERE workspace_id = ? AND created_at BETWEEN ? AND ?", workspace.Id, startFormatted, endFormatted);
-		defer results3.Close()
-		if err != nil {
+		results4, err := db.Query("SELECT id, created_at FROM faxes WHERE workspace_id = ? AND created_at BETWEEN ? AND ?", workspace.Id, startFormatted, endFormatted);
+		if ( err != sql.ErrNoRows && err != nil ) { 
 			fmt.Printf("error running query..\r\n")
 			fmt.Println(err)
 			return
 		}
+		defer results4.Close()
 		var faxId int
-		for results3.Next() {
-			results3.Scan(&faxId, &createdAt)
+		for results4.Next() {
+			results4.Scan(&faxId, &createdAt)
 			change := float64(usedMonthlyFax - 1)
 			centsForFax := baseCosts.FaxPerUsed
-			charge, err := computeAmountToCharge(centsForFax, usedMonthlyFax, change)
+			charge, err := computeAmountToCharge(centsForFax, float64(usedMonthlyFax), change)
 			if err != nil {
 				fmt.Printf("error getting charge..\r\n")
 				fmt.Println(err)
@@ -308,15 +327,27 @@ func runMonthlyBilling() {
 		totalCosts += recordingCosts;
 		totalCosts += faxCosts;
 		totalCosts += monthlyNumberRentals;
+
+		fmt.Printf("Final costs are membership: %f, call tolls: %f, recordings: %f, fax: %f, did rentals: %f, total: %f (cents)\r\n", 
+			membershipCosts,
+			callTolls,
+			recordingCosts,
+			faxCosts,
+			monthlyNumberRentals,
+		totalCosts)
+
+
+		fmt.Printf("Creating debit for user %d, on workspace %d, plan type %s\r\n", user.Id, workspace.Id, workspace.Plan)
 		stmt, err := db.Prepare("INSERT INTO users_debits (`cents`, `status`, `user_id`, `workspace_id`, `created_at`) VALUES ( ?, ?, ?, ?, ?)")
 		if err != nil {
 			fmt.Printf("could not prepare query..\r\n")
 			continue
 		}
 		defer stmt.Close()
-		res, err := stmt.Exec(cents, "INCOMPLETE", workspace.CreatorId, workspace.Id)
+		res, err := stmt.Exec(cents, "INCOMPLETE", workspace.CreatorId, workspace.Id, currentTime)
 		if err != nil {
 			fmt.Printf("error creating debit..\r\n")
+			fmt.Println(err)
 			continue
 		}
 		debitId, err := res.LastInsertId()
@@ -325,10 +356,11 @@ func runMonthlyBilling() {
 			fmt.Println(err)
 			continue
 		}
+		fmt.Printf("Charging user %d, on workspace %d, plan type %s\r\n", user.Id, workspace.Id, workspace.Plan)
 		// try to charge the debit
 		if workspace.Plan == "pay-as-you-go" {
 			remaining := billingInfo.RemainingBalanceCents
-			change :=  float64(remaining) - totalCosts
+			change :=  remaining - totalCosts
 			charge, err := computeAmountToCharge(totalCosts, remaining, change)
 			if err != nil {
 				fmt.Printf("error getting charge..\r\n")
@@ -336,6 +368,7 @@ func runMonthlyBilling() {
 				continue
 			}
 			if ( charge == totalCosts ) { //user has enough credits
+				fmt.Printf("User has enough credits. Charging balance\r\n")
 				stmt, err := db.Prepare("UPDATE users_debits SET status = 'complete', source ='CREDITS' WHERE id = ?")
 				if err != nil {
 					fmt.Printf("could not prepare query..\r\n")
@@ -348,6 +381,7 @@ func runMonthlyBilling() {
 					continue
 				}
 			} else {
+				fmt.Printf("User does not have enough credits. Charging balance as much as possible\r\n")
 				// update debit to reflect exactly how much we can charge
 				stmt, err := db.Prepare("UPDATE users_debits SET status = 'complete', source ='CREDITS', cents = ? WHERE id = ?")
 				if err != nil {
@@ -361,7 +395,7 @@ func runMonthlyBilling() {
 					continue
 				}
 				// try to charge the rest using a card
-
+				fmt.Printf("Charging remainder with card..\r\n")
 
 				cents := int(math.Ceil(charge))
 				err = lineblocs.ChargeCustomer(user, workspace, cents, invoiceDesc)
@@ -376,6 +410,7 @@ func runMonthlyBilling() {
 
 		} else {
 // try to charge the rest using a card
+				fmt.Printf("Charging recurringly with card..\r\n")
 				cents := int(math.Ceil(totalCosts))
 				err = lineblocs.ChargeCustomer(user, workspace, cents, invoiceDesc)
 				if err != nil {
@@ -388,7 +423,8 @@ func runMonthlyBilling() {
 	}
 }
 
-func computeAmountToCharge(fullCentsToCharge float64, monthlyAllowed int, change float64) (float64, error) {
+func computeAmountToCharge(fullCentsToCharge float64, monthlyAllowed float64, change float64) (float64, error) {
+	fmt.Printf("computeAmountToCharge full: %f, monthly allowed: %f, change: %f\r\n", fullCentsToCharge, monthlyAllowed, change);
 	//when total goes below 0, only charge the amount that went below 0
 	if (monthlyAllowed > 0 && change < 0) {
 		percentOfDebit := 1.0;
@@ -406,12 +442,16 @@ func computeAmountToCharge(fullCentsToCharge float64, monthlyAllowed int, change
 		}
 
 		centsToCharge := math.Ceil(float64(fullCentsToCharge) * percentOfDebit)
+		fmt.Printf("computeAmountToCharge result: %f\r\n", centsToCharge);
 		return centsToCharge, nil
 	} else if (monthlyAllowed <= 0) {
+		fmt.Printf("computeAmountToCharge result: %f\r\n", fullCentsToCharge);
 		return fullCentsToCharge, nil
 	} else if (monthlyAllowed > 0 && change >= 0) {
+		fmt.Printf("computeAmountToCharge result: %f\r\n", 0.0);
 		return 0, nil
 	}
+	fmt.Printf("computeAmountToCharge result: %f\r\n", 0.0);
 	return 0, nil
 }
 func runRemoveOldLogs() {
@@ -513,6 +553,7 @@ func runSendBackgroundEmails() {
 		billingInfo, err := lineblocs.GetWorkspaceBillingInfo(workspace)
 		if err != nil {
 			fmt.Printf("Could not get billing info..\r\n")
+			fmt.Println(err)
 			continue
 		}
 
@@ -541,7 +582,7 @@ func runSendBackgroundEmails() {
 				fmt.Println(err)
 				continue
 			}
-			amount := int(math.Round(float64(balance) * percentOfTrigger))
+			amount := math.Round(float64(balance) * percentOfTrigger)
 		
 			if billingInfo.RemainingBalanceCents <= amount {
 				args := make(map[string]string)
